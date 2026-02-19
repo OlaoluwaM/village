2cc018431d3dd4376446827211e8f246d9a2f943
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2026-02-18 23:16:33 -0600

    feat: Scaffold API
    Add ping endpoint and set root endpoint to respond with a ping

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main (main) where
 
-import Lib
+import Network.Wai.Handler.Warp
+import Network.Wai.Logger (withStdoutLogger)
+import Text.Pretty.Simple (pPrint)
+import Village.Api (villageApi)
 
 main :: IO ()
-main = someFunc
+main = withStdoutLogger $ \logger -> do
+    let settings = setPort 8080 $ setLogger logger defaultSettings
+    pPrint "Starting Village API" -- TODO: Replace with something more professional
+    runSettings settings villageApi
---
+++ src/Village/Endpoints/Ping/Handler.hs
@@ -0,0 +1,13 @@
+module Village.Endpoints.Ping.Handler (PingAPI, pingServer) where
+
+import Servant
+
+type PingAPI = RootAPI :<|> ("ping" :> Get '[JSON] NoContent)
+type RootAPI = Get '[JSON] NoContent
+
+-- TODO: Extend this to also check DB status with a dummy query, something like SELECT 1
+pingServer :: Server PingAPI
+pingServer = ping :<|> ping
+
+ping :: Handler NoContent
+ping = pure NoContent
