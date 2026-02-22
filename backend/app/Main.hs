2a868e50ecb56610b20ffe20fdd5219cae7733c3
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2026-02-22 16:51:48 -0600

    feat: Move to a more monorepo like setup

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -2,11 +2,10 @@ module Main (main) where
 
 import Network.Wai.Handler.Warp
 import Network.Wai.Logger (withStdoutLogger)
-import Text.Pretty.Simple (pPrint)
 import Village.Api (villageApi)
 
 main :: IO ()
 main = withStdoutLogger $ \apLogger -> do
     let settings = setPort 8080 $ setLogger apLogger defaultSettings
-    pPrint "Starting Village API" -- TODO: Replace with something more professional
+    putStrLn "Starting Village API" -- TODO: Replace with something more professional
     runSettings settings villageApi
